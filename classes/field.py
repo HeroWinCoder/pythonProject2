c0891da64fc7ca11a6bd2ea6dfd75eb5de5847f6
Gennadiy I <inyaev01@gmail.com>
2024-05-28 12:23:37 +0300

    2Д версию + графики

--- classes/field.py
+++ classes/field.py
@@ -12,22 +12,26 @@ class Field:
     GRID_TITLE = 'Распределение аромата в комнате'
     X_LABEL = 'Вид сверху на комнату, ось X'
     Y_LABEL = 'Вид сверху на комнату, ось Y'
-    DESCRIPTION_TEXT = "Каждый 'шаг' анимации представляет один этап времени, в течение которого духи диффуззируют.\n " \
-                       "Карта цветов показывает концентрацию аромата: от низкой (тёмные тона) до высокой (светлые тона).\n\n"
     length = 100
     width = 100
-    height = 2
     diffusion_rate = 0.3
     dispensers = []
     simulation_time = 10
     frame = 0
     stable_density = 0
 
-    def __init__(self, length, width, height):
+    def __init__(self, length, width):
+        self.y_max = []
+        self.y_average = []
+        self.y_min = []
+        self.x_data = []
+        self.average_dens = None
+        self.min_dens = None
+        self.max_dens = None
+        self.time_text = None
         self.im = None
         self.length = length
         self.width = width
-        self.height = height
         self.grid = np.zeros((width, length))
         self.log_file_name = 'logs/grid_' + time.strftime('%H_%M_%S', time.localtime()) + '.csv'
 
@@ -38,23 +42,33 @@ class Field:
         self.simulation_time = simulation_time
         self.diffusion_rate = diffusion_rate
         self.stable_density = stable_density
-
+    ##В нужн диапазоне в каком то процент простр (70-80%)
+    ##Что-то оптимизировать (пороги, отклонения)
+    ##Результаты эксперимента
     def show_field(self):
-        fig, ax = plt.subplots(figsize=(8, 8))
+        fig, (ax, ax_stat) = plt.subplots(2, 1, figsize=(8, 6))
+
         norm = Normalize(vmin=0, vmax=self.stable_density*2)
         self.im = ax.imshow(self.grid, cmap=cm.hot, norm=norm, interpolation='nearest')
         fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cm.hot), ax=ax, label='Концентрация аромата')
         ax.set_title(self.GRID_TITLE)
-        animation = FuncAnimation(fig, self.update, self.simulation_time, interval=10, repeat=False)
-        plt.xlabel(self.X_LABEL)
-        plt.ylabel(self.Y_LABEL)
-        plt.show()
+        ax.set_xlabel(self.X_LABEL)
+        ax.set_ylabel(self.Y_LABEL)
+        self.time_text = ax.text(-0.1, 1.1, '', transform=ax.transAxes, ha='center', fontsize=12)
 
-    def get_max_portion_from_dispensers(self):
-        max_portion = 0
-        for disp in self.dispensers:
-            max_portion = max(max_portion, disp.portion_per_dispense)
-        return max_portion
+        ax_stat.set_xlim(0, self.simulation_time)
+        ax_stat.set_ylim(0, self.stable_density*2)
+        self.max_dens, = ax_stat.plot([], [], lw=2, label='Максимальная концентрация')
+        self.average_dens, = ax_stat.plot([], [], lw=2, label='Средняя концентрация')
+        self.min_dens, = ax_stat.plot([], [], lw=2, label='Минимальная концентрация')
+        ax_stat.axhline(y=self.stable_density * 1.2, color='r', linestyle='--')
+        ax_stat.axhline(y=self.stable_density * 0.8, color='r', linestyle='--')
+        ax_stat.legend()
+        ax_stat.set_title('Статистика по концентрации')
+
+        animation = FuncAnimation(fig, self.update, self.simulation_time, interval=1, repeat=False)
+        plt.tight_layout()
+        plt.show()
 
     def update(self, frame):
         print("Время: {} сек.".format(self.frame))
@@ -65,8 +79,38 @@ class Field:
         new_grid[new_grid < 0] = 0
         self.grid = new_grid
         self.im.set_array(self.grid)
+        self.time_text.set_text("Время: {} сек.".format(self.frame))
+
+        self.x_data.append(self.frame)
+        self.y_min.append(np.min(self.grid))
+        self.y_average.append(np.mean(self.grid))
+        self.y_max.append(np.max(self.grid))
+        self.max_dens.set_data(self.x_data, self.y_max)
+        self.average_dens.set_data(self.x_data, self.y_average)
+        self.min_dens.set_data(self.x_data, self.y_min)
+
         self.frame += 1
-        return self.im,
+        return self.im, self.time_text, self.max_dens, self.average_dens, self.min_dens
+
+    def simulate(self):
+        time_start = time.time()
+        print("Начало: {}".format(time_start))
+        for i in range(0, self.simulation_time):
+            self.frame = i
+            self.spray_dispensers()
+            self.save_log_grid_data(self.grid)
+            # remains_grid = self.get_remains_grid_bad_version()
+            remains_grid = self.get_remains_grid()
+            new_grid = self.calc_weights(remains_grid)
+            new_grid[new_grid < 0] = 0
+            self.grid = new_grid
+            self.x_data.append(self.frame)
+            self.y_min.append(np.min(self.grid))
+            self.y_average.append(np.mean(self.grid))
+            self.y_max.append(np.max(self.grid))
+        time_end = time.time()
+        print("Конец: {}".format(time_end))
+        print("Время выполнения: {}".format(time_end-time_start))
 
     def spray_dispensers(self):
         for disp in self.dispensers:
@@ -86,6 +130,14 @@ class Field:
 
     def get_remains_grid(self):
         return self.grid * (1-self.diffusion_rate)
+
+    def get_remains_grid_bad_version(self):
+        remain_grid = np.zeros((self.width, self.length))
+        rate = 1-self.diffusion_rate
+        for i in range(0, self.width):
+            for j in range(0, self.length):
+                remain_grid[i, j] = self.grid[i, j] * rate
+        return remain_grid
 
     def calc_weights(self, ratio_grid):
         new_grid = np.zeros((self.width, self.length))
@@ -108,8 +160,8 @@ class Field:
 
     def get_neighbor_divider(self, i, j):
         divider = 4
-        if i == 0 or i == self.width:
+        if i == 0 or i == self.width - 1:
             divider -= 1
-        if j == 0 or j == self.length:
+        if j == 0 or j == self.length - 1:
             divider -= 1
         return divider
--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 import classes.field as field
 import classes.dispenser as dispenser
 
-field = field.Field(5, 5, 2)
-# field.add_dispenser(dispenser.Dispenser([1, 1], 90, 20, [1, 0]))
-# field.add_dispenser(dispenser.Dispenser([1, 18], 30, 20, [0, 1]))
-# field.add_dispenser(dispenser.Dispenser([18, 1], 30, 20, [0, 1]))
-field.add_dispenser(dispenser.Dispenser([3, 3], 30, 20, [1, 0]))
-field.set_field_settings(200, 0.8, 2)
-field.show_field()
-#
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
-# num_points = 100
-# x = np.random.randint(0, 10, num_points)
-# y = np.random.randint(0, 10, num_points)
-# z = np.random.randint(0, 10, num_points)
-# weights = np.random.rand(num_points) * 5
-#
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# cmap = plt.get_cmap('cool')
-# sc = ax.scatter(x, y, z, c=weights, cmap=cmap)
-# plt.colorbar(sc, label='Weights')
-#
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Z')
-# ax.set_title('3D Bar Plot with Weighted Colors')
-#
-# plt.show()
+field = field.Field(20, 10)
+field.add_dispenser(dispenser.Dispenser([5, 5], 300, 30, [3, 0]))
+field.add_dispenser(dispenser.Dispenser([0, 9], 100, 30, [0, -1]))
+# field.add_dispenser(dispenser.Dispenser([9, 0], 30, 1, [0, 1]))
+# field.add_dispenser(dispenser.Dispenser([9, 9], 30, 1, [-1, 0]))
+field.set_field_settings(10000, 0.8, 2)
+field.simulate()
+# field.show_field()
